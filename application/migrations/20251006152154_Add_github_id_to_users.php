6fe617ce4e87b0a1554c30b6d0934d8ad16174a8
Ian <ian.escarro@gmail.com>
2025-10-06 21:33:34 +0800

    Add IDs to migrations

---
+++ application/migrations/20251006152154_Add_github_id_to_users.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * CodeIgniter3
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2024, CodeIgniter3 Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter3
+ * @author	CodeIgniter3 Team
+ * @copyright	Copyright (c) 2014, CodeIgniter3 Team (https://github.com/iescarro/codeigniter3)
+ * @license	https://opensource.org/licenses/MIT	MIT License
+ * @link	https://github.com/iescarro/codeigniter3
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migration_Add_github_id_to_users extends CI_Migration
+{
+	function up()
+	{
+		$fields = array(
+			'github_id' => array(
+				'type' => 'varchar',
+				'constraint' => 255,
+				'null' => TRUE,
+			),
+		);
+		$this->dbforge->add_column('users', $fields);
+	}
+
+	function down()
+	{
+		$this->dbforge->drop_column('users', 'github_id');
+	}
+}
